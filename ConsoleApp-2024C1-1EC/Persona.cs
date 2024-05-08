f817e862a305b71cf9f5c91e498c68f80b8eb87d
christian.slapak <christian.slapak@ort.edu.ar>
2024-05-08 20:54:24 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp-2024C1-1EC/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_2024C1_1EC
+{
+    internal class Persona
+    {
+        public int DNI { get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+
+        public string MostrarDatos()
+        {
+            string texto = "";
+            texto = "Los datos de la persona son: dni_" + DNI + " apellido_" + Apellido + " nombre_" + Nombre;
+            return texto;
+        }
+    }
+}
---
+++ ConsoleApp-2024C1-1EC/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp_2024C1_1EC;
+
+Console.WriteLine("Hola Mundo");
+//Console.ReadKey();
+
+Persona p = new Persona();
+p.DNI = 23456789;
+p.Apellido = "Martinez";
+p.Nombre = "Marcela";
+Console.WriteLine(p.MostrarDatos());
+
